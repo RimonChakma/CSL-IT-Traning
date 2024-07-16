7c588359cb7bee82f1ca51931adb40bebb509082
RimonChakma <rimon.chakma880@gmail.com>
2024-07-16 18:27:52 +0600

    intit

---
+++ android/app/src/main/kotlin/com/example/csl_it_training/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.csl_it_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:csl_it_training/first.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeView(),
+    );
+  }
+
+}
